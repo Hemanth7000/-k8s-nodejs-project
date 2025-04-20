fba410bce4a0f0c401916bdfccf4da5c4c809e76
Hemanth Kumar Reddy <133119694+Hemanth7000@users.noreply.github.com>
2025-04-20 15:04:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('HELLO WORLD FROM K8s Automation with CI/CD - Final Test');
+  res.send('HELLO WORLD FROM K8s Automation with CI/CD - hk Test');
 });
 
 app.listen(PORT, HOST, () => {
